31b990a10637c3daf420b8a430d75b38b3e9be7c
Jason Climo <jason@Jasons-MacBook-Pro.local>
2020-07-21 11:12:44 +1200

    added things

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,22 @@
 // custom js file
 
 (function () {
+	$('#generate').click(function () {
+		var year = document.getElementById('year').value
+		var month = document.getElementById('month').value
+		var day = document.getElementById('day').value
 // GET request for remote image in node.js
 	axios({
 		method: 'get',
-		url: 'https://api.nasa.gov/planetary/apod?api_key=Bmjq2dNI5gHvRoMGbfMsbEWEZ8L3bveRY2bRT69U&date=2002-02-05',
+		url: 'https://api.nasa.gov/planetary/apod?api_key=Bmjq2dNI5gHvRoMGbfMsbEWEZ8L3bveRY2bRT69U&date=' + year + "-" + month + "-" + day,
 		responseType: 'stream'
 	})
   .then(function (response) {
 		console.log(response)
 
-		$('#generate').click(function () {
-			document.getElementById('theImage').src = response.data.hdurl
+		var image = response.data.hdurl
+			document.getElementById('theImage').src = image
+			document.getElementById('imageTitle').innerHTML = response.data.title;
 		})
   })
 })() // iffe ENDS
