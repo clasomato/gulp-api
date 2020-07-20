83d36a4255dd43ace77ba5cad167557ae1bdae77
Jason Climo <jason@Jasons-MacBook-Pro.local>
2020-07-21 10:50:56 +1200

    added the nasa api

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,17 @@
 // custom js file
 
-(function(){
+(function () {
+// GET request for remote image in node.js
+	axios({
+		method: 'get',
+		url: 'https://api.nasa.gov/planetary/apod?api_key=Bmjq2dNI5gHvRoMGbfMsbEWEZ8L3bveRY2bRT69U&date=2002-02-05',
+		responseType: 'stream'
+	})
+  .then(function (response) {
+		console.log(response)
 
-	function makeAjaxRequest () {
-		// GET request for remote image in node.js
-		axios({
-		  method: 'get',
-		  url: 'https://api.nasa.gov/planetary/apod?api_key=0a2OMkeeTLcnN7bPTfoDNdRcGHHTesp4JH88fl13',
-		  responseType: 'stream'
-		}).then(function (response) {
-		   $('#generate').click(function(){
-		   	applyImage(response.data)
-		   });
-		});
-	}
-
-	makeAjaxRequest()
-
-	function applyImage (data) {
-		console.log(data)
-		$('#theImage').attr('src', data.hdurl);
-	}
-
-})(); // iffe ENDS
+		$('#generate').click(function () {
+			document.getElementById('theImage').src = response.data.hdurl
+		})
+  })
+})() // iffe ENDS
